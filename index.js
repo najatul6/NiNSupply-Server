b54d68a579aa0621ad3eedf83fdd9499af0ed4de
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-18 16:59:59 +0600

    remove unnecessary newline in CORS configuration

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://nin-supply.vercel.app/"],
-
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
