0370148defed105d34f5090bba6f541dfbe4d04a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-18 17:03:49 +0600

    fix CORS configuration by removing trailing slash from Vercel origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const uri = process.env.MONGODB_URI;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://nin-supply.vercel.app/"],
+    origin: ["http://localhost:5173", "https://nin-supply.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
