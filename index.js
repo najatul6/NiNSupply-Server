9a4dfb79c19a6b20119904bece9701aad29aeb91
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-21 23:23:37 +0600

    fix: remove unnecessary code in delete cart endpoint

--- index.js
+++ index.js
@@ -127,11 +127,8 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/carts/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await cartsCollection.deleteOne(query);
-      res.send(result);
+    app.delete("/carts/:id",async(req,res)=>{
+      
     })
 
 
