b3f90c05cb0eefe5be07335a86806751474ab3c0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-21 23:19:57 +0600

    fix: standardize quotation marks in delete cart endpoint

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/carts/:id', async (req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartsCollection.deleteOne(query);
