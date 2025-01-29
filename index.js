a23535dfbe29186f397c962eefe10c679e74f1e1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-29 23:22:16 +0600

    feat: enable credentials for CORS configuration

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
     origin: ["http://localhost:5173", "https://nin-supply.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
