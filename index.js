b46b4d3b0f3ab2cc5e7a4eac2ee74c9d377b1e7b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-10 22:41:41 +0600

    fix: set JWT token expiration to 1 hour for consistency with security standards

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
     // JWT
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1m"});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1h"});
       res.send({ token });
     });
 
