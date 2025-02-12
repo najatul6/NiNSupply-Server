a3c46d6b8657269da2cc106796e802dd0850869c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-13 00:56:52 +0600

    fix: reduce JWT expiration time from 1 hour to 1 minute

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
     // JWT
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1m" });
       res.send({ token });
     });
 
