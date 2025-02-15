4ac4e75e3267d83614dc94c6057c17ed1bef47b9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-15 20:23:35 +0600

    feat: add Firebase Admin SDK for enhanced backend functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const admin = require("firebase-admin");
 
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
